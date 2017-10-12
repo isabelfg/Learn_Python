2af6df9d27a09065cd42eb6aaa62bfbdd23804c0
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-10-12 11:00:51 +0200

    Create 01_Basic.py

---
+++ 01_Basic.py
@@ -0,0 +1,63 @@
+## *** Basic level Python ***
+
+print "Welcome to Python!
+
+## Python Syntax: Variables (numbers)
+
+# Write your code below!
+my_variable = 10
+
+# Write your code above!
+print my_variable
+
+## Python Syntax: Booleans (True, False)
+
+# Set the variables to the values listed in the instructions!
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+# Write your code above!
+print my_int
+print my_float
+print my_bool
+
+## Python Syntax: Reassign values
+
+# my_int is set to 7 below. What do you think
+# will happen if we reset it to 3 and print the result?
+my_int = 7
+
+# Change the value of my_int to 3 on line 8!
+my_int = 3
+
+# Here's some code that will print my_int to the console:
+# The print keyword will be covered in detail soon!
+print my_int
+
+## Python Syntax: Whitespaces (structure the code with indentations)
+
+# Bad Format
+def spam():
+eggs = 12
+return eggs
+        
+print spam()
+
+# Good Format(two spaces of indentation)
+def spam():
+  eggs = 12
+  return eggs
+        
+print spam()
+
+
+
+
+
+
+
+
+
+
+
