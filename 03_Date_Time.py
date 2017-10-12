3e20b4b4e752402ee6ecec953feddbe295f86eba
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-10-12 13:25:23 +0200

    Update 03_Date_Time.py

--- 03_Date_Time.py
+++ 03_Date_Time.py
@@ -1,3 +1,25 @@
 ## *** Date and Time ***
 
+## The datetime library (print date and time in a nice format)
 
+# Import the libray
+from datetime import datetime
+
+# Getting the current date (now())
+now = datetime.now()
+print now
+
+# Extract information from date (year, month, day)
+print now.year
+print now.month
+print now.day
+
+# Format (yyyy-mm-dd or mm/dd/yyyy)
+print '%s-%s-%s' % (now.year, now.month, now.day)
+print '%s/%s/%s' % (now.month, now.day, now.year)
+
+# Extract information from tine (HH:MM:SS)
+print '%s:%s:%s' % (now.hour, now.minute, now.second)
+
+# Recap
+print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
