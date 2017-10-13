b9f244cb7e3ff3fef5166e206fa6df7b9ace8824
Isabel Fernandez-Gomez <isabelfg79@gmail.com>
2017-10-13 12:32:27 +0200

    Update 04_Conditionals.py

--- 04_Conditionals.py
+++ 04_Conditionals.py
@@ -201,4 +201,55 @@ def using_control_again():
 print using_control_once()
 print using_control_again()
 
+## IF/ELSE
+
+answer = "'Tis but a scratch!"
+
+def black_knight():
+    if answer == "'Tis but a scratch!":
+        return True
+    else:             
+        return False   # Make sure this returns False
+
+def french_soldier():
+    if answer == "Go away, or I shall taunt you a second time!":
+        return True
+    else:             
+        return False      # Make sure this returns False
+
+ ## ELIF (else if)
+
+def greater_less_equal_5(answer):
+    if answer > 5:
+        return 1
+    elif answer < 5:          
+        return -1
+    else:
+        return 0
+        
+print greater_less_equal_5(4)
+print greater_less_equal_5(5)
+print greater_less_equal_5(6)
+
+## Recap
+
+# Make sure that the_flying_circus() returns True
+def the_flying_circus(answer):
+    if answer == 42:    # Start coding here!
+        # Don't forget to indent
+        print "That is the meaning of universe"
+        return True
+    elif answer > 42:
+        # Keep going here.
+        print "You know nothing Jon Snow"
+        return True
+    else:
+      	print "Tino nino ninoooo"
+      	return True
+
+print the_flying_circus(42)
+print the_flying_circus(72)
+print the_flying_circus(1)
+
+
 
